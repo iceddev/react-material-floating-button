52387eba9b126de1805c163bcb938a5d7ab1909b
Blaine Bublitz <blaine@iceddev.com>
2015-03-18 21:27:07 -0700

    cleanup event handlers on buttons

--- src/main-button.js
+++ src/main-button.js
@@ -3,22 +3,19 @@
 var React = require('react');
 
 var MainButton = React.createClass({
-  handleClick: function() {
-    this.props.onToggleMenu();
-  },
   render: function(){
     var iconResting = 'mfb-component__main-icon--resting ' + this.props.iconResting,
         iconActive = 'mfb-component__main-icon--active ' + this.props.iconActive;
     if(this.props.label){
       return (
-        <a href={this.props.href} className="mfb-component__button--main" onClick={this.handleClick} data-mfb-label={this.props.label}>
+        <a href={this.props.href} className="mfb-component__button--main" onClick={this.props.onToggleMenu} data-mfb-label={this.props.label}>
           <i className={iconResting}></i>
           <i className={iconActive}></i>
         </a>
       );
     } else {
       return (
-        <a href="#" className="mfb-component__button--main" onClick={this.handleClick}>
+        <a href="#" className="mfb-component__button--main" onClick={this.props.onToggleMenu}>
           <i className={iconResting}></i>
           <i className={iconActive}></i>
         </a>
